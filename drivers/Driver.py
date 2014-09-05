eb0f78213c87bca7c8edb67e2cbdcb90ca96331f
egarasky <eric.s.garasky@lawrence.edu>
2014-09-05 12:28:41 -0700

    added time methods to Driver still need to test

--- drivers/Driver.py
+++ drivers/Driver.py
@@ -10,14 +10,22 @@ class Driver(object):
         #chip will provide interface to the hardware so driver can turn itself off and on in physical world
         self.chip = chip
         self.channel = channel
+        if chip is None and type is None:
+            self._abstract = True
+        else:
+            self._abstract = False
+
 
         #initialize state to off
         self.state = "Off"
-        self.state_timer = time.time()
+        self.get_get_state_timer = time.time()
         #time limit caps amount of time driver can be in certain state
         self.time_limit = time_limit
+        #max_time_limit is in seconds in order to ocmpare to time.time easily
         if time_limit:
-            self.max_time = 
+            self.max_time = 0 #limit must still be set, evals to false when checking on method
+        else:
+            self.max_time = -1 #evaluates to True, unlimited with no time_limit, declare still for future limit changes
 
 
         #rename to include current id #, can't pass as optional keyword since those values are binded
@@ -36,23 +44,33 @@ class Driver(object):
     def get_state(self):
         return self.state
 
-    def is_driver(self):
-        return True
-
-    def is_sensor(self):
-        return False
-
     def is_on(self):
         return self.state == "On"
 
-    def get_information(self):
-        return "{driver} on {chip} channel {channel} is {state}".\
-            format(driver = self.name, chip = 'chip', channel = self.channel, state=self.state)
+    def is_off(self):
+        return self.state == 'Off'
+
+    def get_state_time(self):
+        #get difference in times between now and when state was entered
+        seconds = time.time() - self.state_start_time
+        #convert raw data into hour:minutes:seconds time struct
+        minutes = seconds/60
+        seconds = seconds % 60
+        hours = minutes / 60
+        minutes = minutes % 60
+        return (hours, minutes, seconds)
+    
+    def p_state_time(self):
+        "hrs:{hours}min:{mins}sec:{sec}".format(*self.get_state_time())
+
+    def set_time_limit(self, minutes=0, hours=0, seconds=0):
+        max_time_limit = minutes * 60 + hours * 3600 + seconds
+
 
     def turn_on(self): #note and is logical AND, where & is bitwise AND
         if ((self.chip is not None) and (self.channel is not None)): self.chip.on(self.channel)
         self.state = "On"
-        self.begin_state_time = time.time()
+        self.state_start_time = time.time()
         if self.time_limit:
             time.sleep(self.time_limit)
             self.turn_off()
@@ -60,13 +78,18 @@ class Driver(object):
     def turn_off(self):
         if ((self.chip is not None) and (self.channel is not None)): self.chip.off(self.channel)
         self.state = "Off"
-        self.begin_state = time.time()
+        self.state_start_time = time.time()
 
-    def set_control(self, drivercontrol):
-        self.control = drivercontrol
+    def set_control(self, control):
+        self.control = control
 
     def __str__(self):
-        return str(self.state)
+        if self._abstract:
+            return "{driver} is {state} for {time}".format\
+                (driver=self.name, state=self.state, time=self.get_state_time())
+        else:
+            return "{driver} on {chip} channel {channel} is {state}".\
+            format(driver = self.name, chip = 'chip', channel = self.channel, state=self.state)
 
 if __name__=='__main__':
     driver = Driver()
